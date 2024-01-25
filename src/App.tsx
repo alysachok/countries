64aff5126e40fccc4e146e08891bebc0afa9e929
Aleksandra Lysachok <aleksa@Aleksandras-MacBook-Pro-2.local>
2024-01-25 12:51:21 -0500

    logo was moved to public folder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Box, Stack } from "@mui/material";
 import CountriesPage from "../src/Components/Countries";
 import './App.css';
-import countries from "./countries.jpeg";
 
 function App() {
   return (
     <div className="App">
       <Stack flexDirection="row" alignItems="center"  paddingTop="2rem" marginLeft={{xs: "0px", sm: "0px", md: "2rem"}} width="100%" >
           <Box marginRight="2rem">
-          <img src={countries} alt="Globe" width="100px" height="100px"/>
+          <img src={process.env.PUBLIC_URL + '/countries.jpeg'} alt="Globe" width="100px" height="100px"/>
           </Box>
           
           <h1 style={{ color: '#333333' }}>Countries</h1>
